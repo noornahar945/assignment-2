413393980ca88f1556e39e3bae1c2dedb0fa34c2
noornahar945 <noornahar4307@gmail.com>
2026-08-01 19:39:09 +0600

    problem 2 solve alhamdulillah

--- problem-2.js
+++ problem-2.js
@@ -1,11 +1,18 @@
 function isElevatorSafe(weights) {
-  
+ 
+
     if (Array.isArray(weights)) { 
         
-        console.log("valid");
+        // console.log("valid");
+
         let sum = 0;
         for (let i = 0; i<= weights.length - 1; i++) {
             sum += weights[i];
+        }
+        if(sum <= 400){
+            return "true";
+        } else if (sum > 400){
+            return "false";
         }
       
 
@@ -18,4 +25,7 @@ function isElevatorSafe(weights) {
     }
     
 }
-console.log(isElevatorSafe([20,35,40,50]));
+console.log(isElevatorSafe("566677789o90"));
+console.log(isElevatorSafe([400]));
+console.log(isElevatorSafe([20,30,50,60,55]));
+console.log(isElevatorSafe([20,30,50,60,550]));
