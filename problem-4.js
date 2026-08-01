2585433e5de49ac81ec2cd7b2232dfc65224cf36
noornahar945 <noornahar4307@gmail.com>
2026-08-01 22:14:46 +0600

    atkay gesi

--- problem-4.js
+++ problem-4.js
@@ -1,6 +1,13 @@
 function topRatedRestaurant(restaurants) {
     if (Array.isArray(restaurants) && (restaurants.length !== 0)){
         console.log('valid');
+        
+        for(let i=0; i<=restaurants.length-1;i++){
+            console.log(restaurants[i]);
+            if(restaurants[0].rating > restaurants[i-(restaurants.length-1).rating] ){
+                return "good";
+            }
+        }
     }
     else{
         return "Invalid";
@@ -8,4 +15,5 @@ function topRatedRestaurant(restaurants) {
 }
 // console.log(topRatedRestaurant(888));
 console.log(topRatedRestaurant([7,888,90,098,90]));
-console.log(topRatedRestaurant([]));
+console.log(topRatedRestaurant([{name:"Chillox",rating:4.5},{name:"Sultan's Dine",rating:4.8}]));
+
