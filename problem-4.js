cc3bdaed370ff6f6b29ca61fb85c97e6b5e9dfbc
noornahar945 <noornahar4307@gmail.com>
2026-08-01 22:55:46 +0600

    finally done

--- problem-4.js
+++ problem-4.js
@@ -1,17 +1,29 @@
 function topRatedRestaurant(restaurants) {
     if (Array.isArray(restaurants) && (restaurants.length !== 0)){
-        console.log('valid');
-        
-        for(let i=0; i<=restaurants.length-1;i++){
-            console.log(restaurants[i]);
-            if(restaurants[0].rating > restaurants[i-(restaurants.length-1).rating] ){
-                return "good";
-            }
-        }
+        // console.log('valid');
+
+  let largest = restaurants[0].rating;
+  for (let i = 1; i < restaurants.length; i++) {
+    let currentValue = restaurants[i].rating;
+    if (currentValue > largest) {
+      largest = currentValue;
+      return restaurants[i].name.toUpperCase() ;
+
     }
+    
+    
+
+  }
+//   return largest.name;
+
+}
+
+       
     else{
         return "Invalid";
-    }
+    }     
+
+    
 }
 // console.log(topRatedRestaurant(888));
 console.log(topRatedRestaurant([7,888,90,098,90]));
