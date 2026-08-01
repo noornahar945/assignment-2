eab8a860c59a4655c0d54dcfd7cb19158ba426ee
noornahar945 <noornahar4307@gmail.com>
2026-08-01 20:29:06 +0600

    done and passed all case successfully in first attempt , alhamdulillah.

--- problem-3.js
+++ problem-3.js
@@ -1,6 +1,16 @@
 function calculateAiCost(tokensUsed) {
   if (typeof tokensUsed === "number" && tokensUsed >= 0 ){
-    return "valid";
+    // console.log("valid");
+    if(tokensUsed <= 500){
+        return 0;
+    }else if(tokensUsed > 500){
+        let tokenUsed2nd = 0;
+        tokenUsed2nd = Math.floor((tokensUsed - 500)/100);
+        tokenUsed2nd *= 5;
+         return tokenUsed2nd;
+
+    }
+
   }
   else{
     return "Invalid";
@@ -8,4 +18,7 @@ function calculateAiCost(tokensUsed) {
 }
 
 
-console.log(calculateAiCost(0));
+console.log(calculateAiCost(600));
+console.log(calculateAiCost(-10));
+console.log(calculateAiCost(1000));
+console.log(calculateAiCost(500));
