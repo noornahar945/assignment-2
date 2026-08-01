c6e655717f68b19045d63573059647410b631cd6
noornahar945 <noornahar4307@gmail.com>
2026-08-01 23:40:26 +0600

    finally done alhamdulillah

--- problem-4.js
+++ problem-4.js
@@ -1,40 +1,35 @@
 function topRatedRestaurant(restaurants) {
-    if (Array.isArray(restaurants) && (restaurants.length !== 0)){
+    if (Array.isArray(restaurants) && (restaurants.length !== 0)) {
 
 
-  let largest = restaurants[0].rating;
-  for (let i = 1; i < restaurants.length; i++) {
-    let currentValue = restaurants[i].rating;
-    if (currentValue > largest) {
-      largest = currentValue;
-      return restaurants[i].name.toUpperCase() ;
+        let largest = restaurants[0].rating;
+        for (let i = 1; i < restaurants.length; i++) {
+            let currentValue = restaurants[i].rating;
+            if (currentValue > largest) {
+                largest = currentValue;
+                return restaurants[i].name.toUpperCase();
 
-    } 
+            }
+        }
+
+        if (Array.isArray(restaurants) && (restaurants.length === 1)) {
+
+            return restaurants[0].name.toUpperCase();
+        }
+
+
+
     }
-    //  if(restaurants.length === 1){
-    //     return restaurants.name.toUpperCase();
-    //     }
-    if (Array.isArray(restaurants) && (restaurants.length === 1)){
-        // console.log("good");
-        return restaurants[0].name.toUpperCase();
-    }
-    
-    
-
-  }
-   
 
 
-
-       
-    else{
+    else {
         return "Invalid";
-    }     
+    }
 
-    
+
 }
-// console.log(topRatedRestaurant(888));
-// console.log(topRatedRestaurant([7,888,90,098,90]));
-// console.log(topRatedRestaurant([{name:"Chillox",rating:4.5},{name:"Sultan's Dine",rating:4.8}]));
-console.log(topRatedRestaurant([{name:"Chillox",rating:4.5}]));
+console.log(topRatedRestaurant(888));
+console.log(topRatedRestaurant([7, 888, 90,098, 90]));
+console.log(topRatedRestaurant([{ name: "Chillox", rating: 4.5 }, { name: "Sultan's Dine", rating: 4.8 }]));
+console.log(topRatedRestaurant([{ name: "Chillox", rating: 4.5 }]));
 
---
+++ problem-5.js
@@ -0,0 +1,19 @@
+function averageResponseTime(times) {
+  if (Array.isArray(times) === false) {
+        return "Invalid";
+   }
+   
+   if (times.length === 0) {
+        return "Invalid";
+    }
+   
+ let total = 0;
+    for (let i = 0; i <= times.length-1; i++) {
+        total = total + times[i];
+    }
+   
+  
+
+return total / (times.length);
+}
+console.log(averageResponseTime([120, 200, 150, 130]));
